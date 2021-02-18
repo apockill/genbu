32a46defa38de017da9a6d7b17285ab06be1f164
Alex Thiel <apocthiel@gmail.com>
2021-02-17 21:44:28 -0800

    Improve test cleanup and prep

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,7 +6,7 @@ import pytest
 
 from tests.cc_mock import monkeypatch_cc_import
 from tests import cc_mock as cc
-from fleet import state
+from fleet import state_file
 
 
 @pytest.fixture(autouse=True)
@@ -17,9 +17,9 @@ def each_test_setup_teardown():
     cc.fs.files = {}
 
     # Set the statefile writing location
-    old_statefile_directory = state.STATE_DIR
+    old_statefile_directory = state_file.STATE_DIR
     with TemporaryDirectory() as temp_state_dir:
-        state.STATE_DIR = Path(temp_state_dir)
+        state_file.STATE_DIR = Path(temp_state_dir)
         yield
 
-    state.STATE_DIR = old_statefile_directory
+    state_file.STATE_DIR = old_statefile_directory
