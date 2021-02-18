0d990052f6d7918f246732046358d9a11a435047
Alex Thiel <apocthiel@gmail.com>
2021-02-17 22:26:38 -0800

    Moved some code out of quarry.py

--- programs/quarry.py
+++ programs/quarry.py
@@ -8,37 +8,8 @@ from fleet import (
     StateAttr)
 from fleet.math_utils import sign, angle_between
 
-    def _move_in_direction(self, move_sign: int):
-        """Move forwards or backwards in the sign of direction"""
-        position = self.state.position.read()
-        direction = self.state.direction.read()
-        new_position = np.array([
-            round(move_sign * cos(radians(direction))) + position[0],
-            position[1],
-            round(move_sign * sin(radians(direction))) + position[2]
-        ])
 
-        with self.state:
-            if move_sign == 1:
-                turtle.forward()
-            elif move_sign == -1:
-                turtle.back()
-            else:
-                raise ValueError(f"Invalid value for move_sign! {move_sign}")
-            self.state.position.write(new_position)
-
-    def forward(self):
-        # new_position = self.state.position.read() + self.state.direction.read()
-        self._move_in_direction(1)
-
-    def backward(self):
-        self._move_in_direction(-1)
-
-    def turn_right(self):
-        self._turn(90)
-
-    def turn_left(self):
-        self._turn(-90)
+# stateless_turtle.py
 
 
 class NavigationMixin(StatefulTurtle):
