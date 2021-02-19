612ad80a12c9e7490c81bd1f515715d06850f6d9
Alex Thiel <apocthiel@gmail.com>
2021-02-18 18:56:31 -0800

    Renamed stateless -> stateful

--- fleet/__init__.py
+++ fleet/__init__.py
@@ -1,6 +1,6 @@
 from .state_file import StateAttr, StateFile, StateNotAcquiredError
 from .serializable import *
-from .stateless_turtle import (
+from .stateful_turtle import (
     StatefulTurtle,
     TurtleBlockedError,
     Direction,
