ce09478cd25b2f0569893705c1bf4231b7dac482
Alex Thiel <apocthiel@gmail.com>
2021-02-27 21:33:05 -0700

    Updated astar tests

--- tests/test_astar.py
+++ tests/test_astar.py
@@ -1,39 +1,53 @@
 import random
+from typing import List
 
 import numpy as np
 import pytest
 
-from fleet.astar import TurtleAstar
+from fleet.astar import astar
 from fleet.serializable import Map
 
 
 @pytest.mark.parametrize(
-    argnames=("from_dest", "to_dest", "expected_n_moves", "obstacles"),
+    argnames=("from_dest", "to_dest", "expected_n_moves", "obstacles",
+              "obstructed"),
     argvalues=(
             # Test single obstacles
-            ((0, 0, 0), (4, 0, 1), 6, [[1, 0, 0]]),
-            ((0, 0, 0), (4, 0, 1), 8, [[1, 0, 0], [0, 0, 1]]),
+            ((0, 0, 0), (4, 0, 1), 6, [[1, 0, 0]], False),
+            ((0, 0, 0), (4, 0, 1), 8, [[1, 0, 0], [0, 0, 1]], False),
             # Test cornered against 0 grid (the path should work, and travel
             # into the negative direction!)
-            ((0, 0, 0), (4, 0, 1), 8, [[1, 0, 0], [0, 0, 1], [0, 1, 0]]),
+            ((0, 0, 0), (4, 0, 1), 8, [[1, 0, 0], [0, 0, 1], [0, 1, 0]], False),
             # Test being completely blocked leads to no path
-            ((0, 0, 0), (4, 4, 4), 0,
+            ((0, 0, 0), (4, 4, 4), 13,
              [[1, 0, 0], [0, 0, 1], [0, 1, 0], [0, -1, 0], [-1, 0, 0],
-              [0, 0, -1]]),
-            ((2, 2, 2), (4, 4, 4), 0,
+              [0, 0, -1]], True),
+            ((2, 2, 2), (-4, -4, -4), 19,
              [[3, 2, 2], [2, 2, 3], [2, 3, 2], [2, 1, 2], [1, 2, 2],
-              [2, 2, 1]]),
+              [2, 2, 1]], True),
+            # Test when the end goal itself is completely enveloped
+            ((5, 5, 5), (0, 0, 0), 16,
+             [[1, 0, 0], [0, 0, 1], [0, 1, 0], [0, -1, 0], [-1, 0, 0],
+              [0, 0, -1]], True),
             # Test cases with no obstacles
-            ((0, 0, 0), (4, 4, 4), 13, []),
-            ((0, 0, 0), (4, 0, 0), 5, []),
-            ((0, 0, 0), (0, 0, 4), 5, []),
-            ((0, 0, 0), (0, 4, 4), 9, []),
+            ((0, 0, 0), (4, 4, 4), 13, [], False),
+            ((0, 0, 0), (4, 0, 0), 5, [], False),
+            ((0, 0, 0), (0, 0, 4), 5, [], False),
+            ((0, 0, 0), (0, 4, 4), 9, [], False),
             # Test cases that will have from/to points outside of the obstacle
             # min/mx
-            ((-5, -4, -3), (30, 30, 30), 103, [[-4, -4, -3], [-3, -4, -3]]),
+            ((-5, -4, -3), (30, 30, 30), 103, [[-4, -4, -3], [-3, -4, -3]],
+             False),
+            # Test when the end-goal is an obstacle a path is still yielded
+            ((0, 0, 0), (1, 0, 0), 2, [[1, 0, 0]], True),
+            ((0, 0, 0), (1, 1, 1), 4, [[1, 1, 1]], True),
     )
 )
-def test_astar_3d_as_2d(from_dest, to_dest, expected_n_moves, obstacles):
+def test_astar_3d_as_2d(from_dest: List[int],
+                        to_dest: List[int],
+                        expected_n_moves: int,
+                        obstacles: List[List[int]],
+                        obstructed: bool):
     """This is a more opinionated test for validating astar3D with exact
     parameters"""
     map = Map(
@@ -42,12 +56,16 @@ def test_astar_3d_as_2d(from_dest, to_dest, expected_n_moves, obstacles):
         obstacles=obstacles
     )
 
-    path = TurtleAstar(map.obstacles).astar(
-        map.position,
-        to_dest
+    path, path_obstructed = astar(
+        from_pos=map.position,
+        to_pos=to_dest,
+        map=map,
+        obstacle_cost=10
     )
 
+    assert obstructed == path_obstructed
     print(path, obstacles)
+
     # Make sure the path never leads over any obstacles
     for pos in path:
         assert pos not in obstacles
