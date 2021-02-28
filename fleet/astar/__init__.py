36f29095fc313c969a56c6baa16cfdd8315d637d
Alex Thiel <apocthiel@gmail.com>
2021-02-27 21:32:55 -0700

    Cleaned up astar library

--- fleet/astar/__init__.py
+++ fleet/astar/__init__.py
@@ -1,35 +1,27 @@
 from typing import Sequence
 
 from fleet.math_utils import NEIGHBOR_DIRECTIONS
+from fleet import Map
 
 import numpy as np
 from astar import AStar
 
 
 class TurtleAstar(AStar):
-    def __init__(self, obstacles: np.ndarray, e_admissibility=2):
+    def __init__(self, map: Map, e_admissibility: float, obstacle_cost: int):
         """
         :param obstacles: A numpy array of obstacles. Obtained by map.obstacles
         :param e_admissibility: A multiplier to significantly speed up A*.
         A value of '1' guarrentees the most optimal path, but has compute
         implications. The larger the number, the more 'admissible' the path may
         be to detours.
+        :param obstacle_cost: Think of this as "how many blocks would I rather
+        move around instead of breaking a block"
         """
-        self.obstacles = obstacles
+        self.map = map
         self.heuristic_multiplier = e_admissibility
+        self.obstacle_cost = obstacle_cost
         super().__init__()
-
-    def astar(self, start: Sequence, goal: Sequence, reversePath=False):
-        if isinstance(start, np.ndarray):
-            start = start.tolist()
-        if isinstance(goal, np.ndarray):
-            goal = goal.tolist()
-
-        path = super().astar(tuple(start), tuple(goal), reversePath=reversePath)
-        if path is None:
-            return []
-        else:
-            return list(path)
 
     def is_goal_reached(self, current, goal):
         """ returns true when we can consider that 'current' is the goal"""
@@ -47,19 +39,39 @@ class TurtleAstar(AStar):
         n2 is guaranteed to belong to the list returned by the call to
         neighbors(n1).
         """
-        return 1
+        if self.map.is_known_obstacle(n1) or self.map.is_known_obstacle(n2):
+            return self.obstacle_cost
+        else:
+            return 1
 
     def neighbors(self, node):
         """For a given node, returns (or yields) the list of its neighbors."""
-        # get neighbor coordinates, checking also map limits (cell != 0 = wall)
-        for md in NEIGHBOR_DIRECTIONS:
-            n = (node[0] + md[0], node[1] + md[1], node[2] + md[2])
+        return ((node[0] + md[0], node[1] + md[1], node[2] + md[2])
+                for md in NEIGHBOR_DIRECTIONS)
 
-            # Compute 3D mask
-            xmask = (self.obstacles[..., 0] == n[0]) \
-                    & (self.obstacles[..., 1] == n[1]) \
-                    & (self.obstacles[..., 2] == n[2])
+def astar(from_pos: Sequence,
+          to_pos: Sequence,
+          map: Map,
+          obstacle_cost=10,
+          e_admissibility=2):
+    if isinstance(from_pos, np.ndarray):
+        from_pos = from_pos.tolist()
+    if isinstance(to_pos, np.ndarray):
+        to_pos = to_pos.tolist()
 
-            # Only return n if it isn't an obstacle
-            if ~(self.obstacles[xmask].any()):
-                yield n
+    path = (TurtleAstar(map=map,
+                        e_admissibility=e_admissibility,
+                        obstacle_cost=obstacle_cost)
+        .astar(
+        start=tuple(from_pos),
+        goal=tuple(to_pos)))
+
+    path = list(path)
+    for pos in path:
+        if map.is_known_obstacle(pos):
+            return path, True
+
+    return path, False
+
+
+__all__ = ["astar"]
