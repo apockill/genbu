d7b438bd4b9a7bb940d0dbef66f74382973f39e3
Alex Thiel <apocthiel@gmail.com>
2021-02-27 18:27:08 -0700

    Replaced astar algorithm

--- fleet/astar/__init__.py
+++ fleet/astar/__init__.py
@@ -1,223 +1,65 @@
+from typing import Sequence
+
+from fleet.math_utils import NEIGHBOR_DIRECTIONS
+
 import numpy as np
-
-from fleet.serializable import Map
-from fleet import math_utils
+from astar import AStar
 
 
-class Astar2D:
-    class Node:
-        '''
-        Node class used to create the graph
-        '''
+class TurtleAstar(AStar):
+    def __init__(self, obstacles: np.ndarray, e_admissibility=2):
+        """
+        :param obstacles: A numpy array of obstacles. Obtained by map.obstacles
+        :param e_admissibility: A multiplier to significantly speed up A*.
+        A value of '1' guarrentees the most optimal path, but has compute
+        implications. The larger the number, the more 'admissible' the path may
+        be to detours.
+        """
+        self.obstacles = obstacles
+        self.heuristic_multiplier = e_admissibility
+        super().__init__()
 
-        def __init__(self, prev_node=None, pos=None):
-            '''
-            :param prev_node: Node element, needed to reconstruct the path
-            :param pos: position in the  graph
-            '''
-            self.pos = pos
-            self.H = None
-            self.G = None
-            self.F = None
-            self.prevNode = prev_node
+    def astar(self, start: Sequence, goal: Sequence, reversePath=False):
+        if isinstance(start, np.ndarray):
+            start = start.tolist()
+        if isinstance(goal, np.ndarray):
+            goal = goal.tolist()
 
-        def __eq__(self, in_node):
-            '''
-            New meaning for graph search
-            :param in_node: input node
-            :return: self position equal to new one
-            '''
-            return self.pos == in_node.pos
-
-        def __lt__(self, other):
-            '''
-            New meaning for sorting
-            :param other: other Node
-            :return:
-            '''
-            self.F < other.F
-
-    def __init__(self):
-        '''
-        Init stuff
-        '''
-        self.start_point = (0, 0)
-        self.end_point = (0, 0)
-        self.map = []
-        self.path = list()
-
-        # grid with and height
-        self.gw = 0
-        self.gh = 0
-
-        # neighbors currently 4, no diagonals
-        self.moving_dirs = self.get_neighbors_directions()
-
-    def set_neighbors_directions(self, dirs=None):
-        '''
-        Define new neighbor directions
-        :param dirs: list of new poitions
-        :return: list of positional directions
-        '''
-        self.moving_dirs = dirs
-
-    def generate_path(self, map: Map, to_pos, from_pos=None):
-        '''
-        A star implementation
-        :param from_pos: start
-        :param to_pos: goal
-        :param grid_map: input map
-        :param shape: search space shape (i.e. 2d map (10,10))
-        :return: None
-        '''
-        self.load_map(map)
-
-        offset = map.zero_offset + [-1, -1, -1]
-        from_pos = np.array(from_pos if from_pos is not None else map.position)
-        from_pos -= offset
-        to_pos = np.array(to_pos) - offset
-        self.start_point = tuple(from_pos.tolist())
-        self.end_point = tuple(to_pos.tolist())
-
-        open_list = []
-        closed_list = []
-
-        # prepare open list
-        start_node = self.Node(None, self.start_point)
-        end_node = self.Node(None, self.end_point)
-
-        self.__compute_cost(start_node, end_node, start_node)
-        open_list.append(start_node)
-
-        # check open list items
-        while len(open_list) > 0:
-
-            # get the smallest F node
-            open_list.sort()
-
-            cn = open_list.pop(0)
-            closed_list.append(cn)
-
-            # test if the end_node was reached, if yes exit
-            if cn == end_node:
-                self.path = self.get_path(cn)
-                break
-
-            # get neighbours
-            neighbors = self.get_neighbors(cn)
-
-            # test neighbors and add to open list
-            for n in neighbors:
-
-                # make node object from coordinates
-                nn = self.Node(cn, n)
-
-                # calculate the costs from start / end
-                self.__compute_cost(start_node, end_node, nn)
-
-                # check if neighbor is on closed list
-                if nn in closed_list:
-                    continue
-
-                # check if neighbor is on the open list
-                if nn not in open_list:
-                    open_list.append(nn)
-
-        if len(self.path):
-            return np.array(self.path) + offset
+        path = super().astar(tuple(start), tuple(goal), reversePath=reversePath)
+        if path is None:
+            return []
         else:
-            return np.array([])
+            return list(path)
 
-    @staticmethod
-    def __compute_cost(start_node, end_node, current_node):
-        '''
-        Calculate the cost from start + heuristics (end)
-        :param start_node: is the starting position
-        :param end_node: goal
-        :param current_node: graph current node
-        :return:
-        '''
+    def is_goal_reached(self, current, goal):
+        """ returns true when we can consider that 'current' is the goal"""
+        return current == goal
 
-        current_node.G = np.linalg.norm(
-            np.array(start_node.pos) - np.array(current_node.pos))
-        current_node.H = np.linalg.norm(
-            np.array(end_node.pos) - np.array(current_node.pos))
-        current_node.F = current_node.G + current_node.H
+    def heuristic_cost_estimate(self, current, goal):
+        """Computes the estimated (rough) distance between a node and the goal.
+        The second parameter is always the goal."""
+        return np.abs(
+            np.array(goal) - current).sum() * self.heuristic_multiplier
 
-    def get_path(self, current_node):
-        '''
-        Recreate the path from end till beginning
-        :param current_node: is the actual (last) node = end
-        :return: list of coordinates from start till the end
-        '''
+    def distance_between(self, n1, n2):
+        """Gives the real distance between two adjacent nodes n1 and n2
+        (i.e n2 belongs to the list of n1's neighbors).
+        n2 is guaranteed to belong to the list returned by the call to
+        neighbors(n1).
+        """
+        return 1
 
-        self.path = list()
-
-        while current_node is not None:
-            self.path.append(current_node.pos)
-            # take the previous node
-            current_node = current_node.prevNode
-        return np.array(self.path[::-1])
-
-
-class Astar3D(Astar2D):
-    def __init__(self):
-        '''
-        Init stuff
-        '''
-
-        Astar2D.__init__(self)
-        self.start_point = (0, 0, 0)
-        self.end_point = (0, 0, 0)
-
-        self.gw = 0
-        self.gh = 0
-        self.gd = 0
-
-        self.map = []
-        self.path = list()
-
-        Astar2D.set_neighbors_directions(self, self.get_neighbors_directions())
-        self.moving_dirs = self.get_neighbors_directions()
-
-    def get_neighbors_directions(self):
-        '''
-        Define neighbor directions
-        :return: list of positional directions
-        '''
-        return math_utils.NEIGHBOR_DIRECTIONS
-
-    def load_map(self, map: Map):
-        '''
-        Set local map
-        :param map: the map
-        :param shape: the shape (limits) of the 3d point cloud
-        :return: None
-        '''
-
-        self.gw = map.shape[0] + 2
-        self.gh = map.shape[1] + 2
-        self.gd = map.shape[2] + 2
-        self.map = map.offset_points + [1, 1, 1]
-
-    def get_neighbors(self, node):
-        '''
-        Return a list of neighbors
-        :param node: input node
-        :return: list of neighbor coordinates
-        '''
-
-        ret = []
-
+    def neighbors(self, node):
+        """For a given node, returns (or yields) the list of its neighbors."""
         # get neighbor coordinates, checking also map limits (cell != 0 = wall)
-        for md in self.moving_dirs:
-            n = (node.pos[0] + md[0], node.pos[1] + md[1], node.pos[2] + md[2])
+        for md in NEIGHBOR_DIRECTIONS:
+            n = (node[0] + md[0], node[1] + md[1], node[2] + md[2])
 
-            # compute 3D mask
-            xmask = (self.map[..., 0] == n[0]) & (self.map[..., 1] == n[1]) & (
-                    self.map[..., 2] == n[2])
+            # Compute 3D mask
+            xmask = (self.obstacles[..., 0] == n[0]) \
+                    & (self.obstacles[..., 1] == n[1]) \
+                    & (self.obstacles[..., 2] == n[2])
 
-            if 0 <= n[0] < self.gw and 0 <= n[1] < self.gh and 0 <= n[
-                2] < self.gd and (self.map[xmask].any()):
-                ret.append(n)
-        return ret
+            # Only return n if it isn't an obstacle
+            if ~(self.obstacles[xmask].any()):
+                yield n
