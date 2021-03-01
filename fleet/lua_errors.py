02cc104f69bd68acd11813d73333812322f8afaf
Alex Thiel <apocthiel@gmail.com>
2021-02-28 22:25:06 -0700

    Added more lua errors, and easy functions for wrapping lua calls with error handling

--- fleet/lua_errors.py
+++ fleet/lua_errors.py
@@ -9,24 +9,39 @@ if TYPE_CHECKING:
 class TurtleBlockedError(LuaException):
     """Raised when the turtles path is blocked"""
 
-    def __init__(self, message, direction: 'Direction'):
+    def __init__(self, message, direction: 'Direction' = None):
         super().__init__(message)
         self.direction = direction
         """The direction of the blocking block"""
 
 
 class UnbreakableBlockError(LuaException):
-    """Called when the turtle tries to mine a block it can't break"""
+    """Raised when the turtle tries to mine a block it can't break"""
 
-    def __init__(self, message, direction: 'Direction'):
+    def __init__(self, message, direction: 'Direction' = None):
         super().__init__(message)
         self.direction = direction
         """The direction of the unbreakable block"""
 
 
+class OutOfFuelError(LuaException):
+    """Raised when the turtle tries to move but is out of fuel"""
+
+
+class ItemNotCombustibleError(LuaException):
+    """Raised when turtle.refuel() is called on an item that isn't combustible"""
+
+
+class NoItemsToCombustError(LuaException):
+    """Raised when turtle.refuel() is called on an empty slot"""
+
+
 FROM_LUA: Dict[str, Any] = {
     "Movement obstructed": TurtleBlockedError,
-    "Unbreakable block detected": UnbreakableBlockError
+    "Unbreakable block detected": UnbreakableBlockError,
+    "Out of fuel": OutOfFuelError,
+    "Items not combustible": ItemNotCombustibleError,
+    "No items to combust": NoItemsToCombustError,
 }
 """Map LuaErrors to this libraries errors. These are mapped based on the 
 messages in the LuaError, hence the key to the dict is a string representing 
@@ -37,12 +52,20 @@ TO_LUA: Dict[Any, str] = {value: key for key, value in FROM_LUA.items()}
 """Useful for generating LuaExceptions in tests"""
 
 
-def raise_mapped_error(exc: LuaException, message=None, **kwargs):
+def map_error(exc: LuaException, message=None, **kwargs):
     if exc.message in FROM_LUA:
         message = message if message else exc.message
-        raise FROM_LUA[exc.message](message, **kwargs)
+        return FROM_LUA[exc.message](message, **kwargs)
     else:
-        raise exc
+        return exc
+
+
+def convert_errors(fn, *args, **kwargs):
+    """Try a function and convert any LuaExceptions"""
+    try:
+        fn(*args, **kwargs)
+    except LuaException as e:
+        raise map_error(e, message=e.message)
 
 
 __all__ = ["FROM_LUA",
