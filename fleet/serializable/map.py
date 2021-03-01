ad03a4087d6a7f591040ebbd969fb3edaaf0892c
Alex Thiel <apocthiel@gmail.com>
2021-02-28 22:22:56 -0700

    Convert map to holding positions of known obstacles, rather than 'free spots'

--- fleet/serializable/map.py
+++ fleet/serializable/map.py
@@ -8,66 +8,57 @@ from fleet.serializable.base import BaseSerializable
 
 
 class Map(BaseSerializable):
-    def __init__(self, position: tuple,
+    def __init__(self, position: Union[Tuple[int], List[int]],
                  direction: int,
-                 points: np.ndarray = None):
+                 obstacles: Union[np.ndarray, List[List[int]]] = None):
         """
         :param position: The current position in the map
         :param direction: The direction the turtle is facing in the map
-        :param points: The point cloud of all points ever traveled
+        :param obstacles: The point cloud of all obstacles ever encountered
         """
-        self.position = np.array(position)
-        self.points = np.array([position]) if points is None else points
-        self.direction = direction
+        self.position: np.ndarray = np.array(position)
+        self.obstacles: np.ndarray = (
+            np.zeros(shape=(0, 3), dtype=np.int8)
+            if obstacles is None or len(obstacles) == 0 else
+            np.array(obstacles))
+        self.direction: int = direction
 
     def __repr__(self):
-        return f"Map(n_points={len(self.points)}, " \
+        return f"Map(n_obstacles={len(self.obstacles)}, " \
                f"position={self.position}," \
                f"direction={self.direction})"
 
-    @property
-    def shape(self) -> np.ndarray:
-        return np.amax(self.points, axis=0) - np.amin(self.points, axis=0)
-
-    @property
-    def offset_points(self):
-        return self.points.copy() - self.zero_offset
-
-    @property
-    def zero_offset(self):
-        return np.amin(self.points, axis=0)
-
-    def nearest_known_position(self, position: np.ndarray):
-        """Returns the nearest known position"""
-        tree = cKDTree(self.points)
-        dist, indexes = tree.query(position)
-        return self.points[indexes]
-
-    def is_known_position(self, position: np.ndarray):
-        """Returns whether this is a known point"""
-        return any((self.points == position).all(1))
-
     def move_to(self, position: Union[np.ndarray, Tuple]):
         """Move to an adjacent block relative to the current position"""
-        assert is_adjacent(position, self.position)
+        assert is_adjacent(position, self.position), \
+            "You can't move to a non adjacent position!"
 
         position = np.array(position)
-        self._add_point(position)
+        self.remove_obstacle(position)
         self.position = position
 
-    def _add_point(self, position: np.ndarray):
-        if self.is_known_position(position):
-            # This point is already registered. No need to register it!
+    def remove_obstacle(self, position):
+        """Clear an obstacle if it exists in the array"""
+        delete_row = np.where((self.obstacles == position).all(axis=1))
+        self.obstacles = np.delete(self.obstacles, delete_row, axis=0)
+
+    def add_obstacle(self, position: Union[np.ndarray, List]):
+        if self.is_known_obstacle(position):
+            # This obstacle is already registered. No need to register it!
             return
-        self.points = np.vstack((self.points, position))
+        self.obstacles = np.vstack((self.obstacles, position))
+
+    def is_known_obstacle(self, position: np.ndarray):
+        """Returns whether this is a known obstacle"""
+        return any((self.obstacles == position).all(axis=1))
 
     def to_dict(self) -> Dict[str, Any]:
         return {"position": self.position.tolist(),
                 "direction": self.direction,
-                "points": self.points.tolist(), }
+                "obstacles": self.obstacles.tolist()}
 
     @classmethod
     def from_dict(cls, obj: Dict[str, Any]) -> 'Map':
-        return cls(points=np.array(obj["points"]),
-                   position=np.array(obj["position"]),
+        return cls(obstacles=np.array(obj["obstacles"]),
+                   position=obj["position"],
                    direction=obj["direction"])
