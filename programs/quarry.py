94f9a563b868668b42f56f661820207a3f2a7653
Alex <alex.thiel@aotu.ai>
2021-03-10 22:13:43 -0800

    QuarryTurtle is in a working state!

--- programs/quarry.py
+++ programs/quarry.py
@@ -4,117 +4,123 @@ import numpy as np
 
 from fleet import (
     routines,
-    Astar3D,
-    StatefulTurtle,
     StateAttr,
     PromptStateAttr,
     lua_errors)
 from fleet.math_utils import sign, angle_between, is_adjacent
 
 
-class NavigationMixin(StatefulTurtle):
-    """Adds high-level methods helpful for moving around"""
+def parse_position(val: str):
+    return np.array(list(map(int, val.split(" "))))
 
-    def move_toward_destructively(self, to_pos):
+
+# quarry.py
+class QuarryTurtle(NavigationTurtle):
+    def __init__(self):
+        super().__init__()
+        with self.state:
+            initial_loc = self.state.map.read().position
+        self.state.fuel_loc = PromptStateAttr(
+            self.state, "fuel_loc",
+            parser=parse_position,
+            default=initial_loc)
+
+        with self.state:
+            hx, hy, hz = self.state.fuel_loc.read()
+
+        self.state.dump_loc = PromptStateAttr(
+            self.state, "dump_loc",
+            parser=parse_position,
+            default=np.array([hx + 2, hy, hz]))
+        self.state.mining_x1z1 = PromptStateAttr(
+            self.state, "mining_x1z1",
+            parser=parse_position,
+            default=np.array((hx + 10, hz)))
+        self.state.mining_x2z2 = PromptStateAttr(
+            self.state, "mining_x2z2",
+            parser=parse_position,
+            default=np.array((hx + 110, hz + 100)))
+        self.state.dig_height = PromptStateAttr(
+            self.state, "dig_height",
+            parser=int,
+            default=int(hy))
+        self.state.dig_depth = PromptStateAttr(
+            self.state, "dig_depth",
+            parser=int,
+            default=-1)
+        self.state.columns_finished = StateAttr(
+            self.state, "columns_finished",
+            default=[])
+        self.state.columns_started = StateAttr(
+            self.state, "columns_started",
+            default=[])
+        """This will hold a list of [x, z] areas of columns that have already 
+        been dug all the way down."""
+
+        # Clean up any broken state (columns that were started but not finished
+        with self.state as state:
+            started = state.columns_started.read()
+            finished = state.columns_finished.read()
+            started = [c for c in started if c in finished]
+            state.columns_started.write(started)
+
+    def step(self, state):
+        routines.maybe_refuel(self, state.fuel_loc.read())
+        routines.dump_if_full(self, state.dump_loc.read(), range(2, 16 + 1))
+
+        columns_started = state.columns_started.read()
+        columns_finished = state.columns_finished.read()
+        next_column = self.get_next_column(
+            x1z1=state.mining_x1z1.read(),
+            x2z2=state.mining_x2z2.read(),
+            finished_columns=columns_finished
+        )
+
+        if next_column is None:
+            # If the robot is done, go home!
+            self.move_toward(state.fuel_loc.read())
+            return
+
+        x, z = next_column
+
+        if next_column not in columns_started:
+            # Then move to the correct X/Z
+            self.move_toward([x, state.dig_height.read(), z])
+            started = columns_started
+            started.append(next_column)
+            state.columns_started.write(started)
+
+        with state:
+            curr_x, curr_y, curr_z = state.map.read().position
+
+        # Okay! The column has been started, better dig down!
+        def mark_column_finished():
+            """Mark a column as finished"""
+            columns_finished.append(next_column)
+            state.columns_finished.write(columns_finished)
+            raise StepFinished()
+
+        if curr_y <= state.dig_depth.read():
+            mark_column_finished()
+
         try:
-            self.move_toward(to_pos)
-        except lua_errors.TurtleBlockedError as e:
-            self.dig_towards(e.direction)
+            self.move_toward(
+                to_pos=[x, curr_y - 1, z],
+                destructive=True)
+        except lua_errors.UnbreakableBlockError:
+            mark_column_finished()
 
-    def move_toward(self, to_pos):
-        """Make a move in one of these directions, turning automatically"""
-        path = self.generate_path(to_pos)
-        self.move_along_path(path)
+    def get_next_column(self, x1z1, x2z2, finished_columns):
+        from_x, to_x = sorted([x1z1[0], x2z2[0]])
+        from_z, to_z = sorted([x1z1[1], x2z2[1]])
+        try:
+            return next(
+                ([x, z]
+                 for x in range(from_x, to_x + 1)
+                 for z in range(from_z, to_z + 1)
+                 if [x, z] not in finished_columns))
+        except StopIteration:
+            return None
 
-    def move_along_path(self, path: List[int]):
-        if len(path) == 0:
-            return
-
-        with self.state as state:
-            map = state.map.read()
-        curr_pos = map.position.tolist()
-        if map.position.tolist() in path and len(path) > 1:
-            path = path[path.index(curr_pos) + 1:]
-
-        # Try to movZe toward that direction
-        next_pos = np.array(path[0])
-        dist = next_pos - curr_pos
-        assert abs(dist.sum()) == 1, \
-            "The move must be to an adjacent path!"
-
-        if dist[1] != 0:
-            self.move_vertically(dist[1])
-        elif dist[0] != 0 or dist[2] != 0:
-            self.turn_toward(next_pos)
-            self.forward()
-
-    def generate_path(self, to_pos, from_pos=None):
-        """
-        :param to_pos: The position to pathfind to
-        :param from_pos: The position from which to pathfind to. If None, it
-        will choose the turtles current position.
-        :return:
-        """
-        with self.state as state:
-            map = state.map.read()
-
-        # Move along the shortest axis first, then the longest
-        astar = Astar3D()
-        nearest_known = map.nearest_known_position(target_position)
-        path = astar.generate_path(
-            map=map,
-            end_point=nearest_known)[1:]
-
-        if len(path) == 0 and (map.position != target_position).any():
-            # If we've reached the nearest_known location already
-            self.explore_towards(target_position)
-        else:
-            next_move = path[0]
-            x, y, z = next_move
-            if x == map.position[0] and z == map.position[2]:
-                assert sign(y - map.position[1]) != 0
-                self.move_vertically(sign(y - map.position[1]))
-            else:
-                self.turn_toward(next_move)
-                self.forward()
-
-        if len(path) == 0:
-            path = []
-
-        # Astar only gets you to the nearest known spot. After Astar you need
-        # do some naive "exploration" along each relevant axis.
-        while not len(path) or not path[-1] == to_pos:
-            curr_pos = path[-1] if len(path) else from_pos
-            dist = np.array(to_pos) - curr_pos
-            curr_x, curr_y, curr_z = curr_pos
-            if dist[0] != 0:
-                path.append([curr_x + sign(dist[0]), curr_y, curr_z])
-            elif dist[2] != 0:
-                path.append([curr_x, curr_y, curr_z + sign(dist[2])])
-            elif dist[1] != 0:
-                path.append([curr_x, curr_y + sign(dist[1]), curr_z])
-            else:
-                raise RuntimeError(f"How was there no move?"
-                                   f"{path} {curr_pos} {to_pos}")
-
-        return path
-
-    def turn_toward(self, to_pos):
-        """Turn toward the target_position
-        """
-        map = self.state.map.read()
-        x_dist, _, z_dist = to_pos.copy() - map.position
-        if x_dist == 0 and z_dist == 0:
-            return
-
-        unit_vector = [sign(x_dist), 0, sign(z_dist)]
-        turn_angle = angle_between(np.array([0, 0, 0]), unit_vector)
-        turn_direction = sign(turn_angle % 360 - map.direction)
-
-        if turn_direction != 0:
-            if turn_direction > 0:
-                self.turn_right()
-            elif turn_direction < 0:
-                self.turn_left()
 
 QuarryTurtle().run()
