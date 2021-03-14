d7df31c4c2c834ead12a19b9b2fb6a553767f9f8
Alex <alex.thiel@aotu.ai>
2021-03-13 22:52:18 -0800

    Improved quarrybot stability and safety

--- programs/quarry.py
+++ programs/quarry.py
@@ -8,7 +8,8 @@ from fleet import (
     PromptStateAttr,
     lua_errors,
     NavigationTurtle,
-    StepFinished
+    StepFinished,
+    math_utils
 )
 
 
@@ -70,11 +71,19 @@ class QuarryTurtle(NavigationTurtle):
         routines.maybe_refuel(self, state.fuel_loc.read())
         routines.dump_if_full(self, state.dump_loc.read(), range(2, 16 + 1))
 
+        fuel_loc = state.fuel_loc.read()
         columns_started = state.columns_started.read()
         columns_finished = state.columns_finished.read()
+        mining_x1z1 = state.mining_x1z1.read()
+        mining_x2z2 = state.mining_x2z2.read()
+        dig_height = state.dig_height.read()
+        dig_depth = state.dig_depth.read()
+        curr_pos = state.map.read().position
+
         next_column = self.get_next_column(
-            x1z1=state.mining_x1z1.read(),
-            x2z2=state.mining_x2z2.read(),
+            x1z1=mining_x1z1,
+            x2z2=mining_x2z2,
+            home_pos=fuel_loc,
             finished_columns=columns_finished
         )
 
@@ -85,15 +94,25 @@ class QuarryTurtle(NavigationTurtle):
 
         x, z = next_column
 
-        if next_column not in columns_started:
-            # Then move to the correct X/Z
-            self.move_toward([x, state.dig_height.read(), z])
+        within_dig_volume = math_utils.within_bounding_points(
+            point=curr_pos,
+            bp1=(mining_x1z1[0], dig_depth, mining_x1z1[1]),
+            bp2=(mining_x2z2[0], dig_height, mining_x2z2[1])
+        )
+
+        if (next_column not in columns_started
+                or not within_dig_volume):
+            # The turtle will move to the start of the next column if it
+            # a) is above the height of the dig volume (ie, if it grabs fuel
+            #    or dumps its items)
+            # b) it simply hasn't started the next column
+            column_start_pos = np.array((x, dig_height, z))
+
+            self.move_toward(column_start_pos,
+                             destructive=within_dig_volume)
             started = columns_started
             started.append(next_column)
             state.columns_started.write(started)
-
-        with state:
-            curr_x, curr_y, curr_z = state.map.read().position
 
         # Okay! The column has been started, better dig down!
         def mark_column_finished():
@@ -107,22 +126,26 @@ class QuarryTurtle(NavigationTurtle):
 
         try:
             self.move_toward(
-                to_pos=[x, curr_y - 1, z],
-                destructive=True)
+                to_pos=[x, curr_pos[1] - 1, z],
+                destructive=within_dig_volume)
         except lua_errors.UnbreakableBlockError:
             mark_column_finished()
 
-    def get_next_column(self, x1z1, x2z2, finished_columns):
+    def get_next_column(self, home_pos, x1z1, x2z2, finished_columns):
+        """Return the next column. They are sorted by distance from home"""
         from_x, to_x = sorted([x1z1[0], x2z2[0]])
         from_z, to_z = sorted([x1z1[1], x2z2[1]])
-        try:
-            return next(
-                ([x, z]
-                 for x in range(from_x, to_x + 1)
-                 for z in range(from_z, to_z + 1)
-                 if [x, z] not in finished_columns))
-        except StopIteration:
+
+        hx, _, hz = home_pos
+        columns = [[x, z]
+                   for x in range(from_x, to_x + 1)
+                   for z in range(from_z, to_z + 1)
+                   if [x, z] not in finished_columns]
+        if not len(columns):
             return None
 
+        columns.sort(
+            key=lambda c: (hx - c[0]) ** 2 + (hz - c[1]) ** 2)
+        return columns[0]
 
 QuarryTurtle().run()
