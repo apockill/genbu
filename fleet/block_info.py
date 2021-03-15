7f4f452aa4fc0b88633efd04c874f5196f1ead25
Alex <alex.thiel@aotu.ai>
2021-03-14 19:05:55 -0700

    Add regex helper for block name matching

--- fleet/block_info.py
+++ fleet/block_info.py
@@ -3,6 +3,8 @@ are useful for things.
 
 Each list of a list of regex matches for block names
 """
+from typing import List, Union
+import re
 
 do_not_mine = [
     r"computercraft:.*",
@@ -11,3 +13,10 @@ do_not_mine = [
 ]
 """This is a list of items a turtle should never mine, under any circumstance.
 """
+
+
+def name_matches_regexes(block_name: Union[bytes, str], regex_list: List[str]):
+    combined_regex = '(?:% s)' % '|'.join(regex_list)
+    if isinstance(block_name, bytes):
+        block_name = str(block_name, encoding="utf-8")
+    return re.match(combined_regex, block_name)
