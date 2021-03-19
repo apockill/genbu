d746cb03071634f85fe6a319afcd46e78a845c3b
Alex <alex.thiel@aotu.ai>
2021-03-19 09:48:34 -0700

    Add a trigger slot parameter to dump_if_full

--- fleet/routines/dump_items.py
+++ fleet/routines/dump_items.py
@@ -3,14 +3,16 @@ from cc import turtle
 from fleet.navigation_turtle import NavigationTurtle, Direction
 
 
-def dump_if_full(nav_turtle: NavigationTurtle, dump_spot, dump_slots):
+def dump_if_full(nav_turtle: NavigationTurtle, dump_spot, dump_slots,
+                 trigger_slot=16):
     """Goes to a dump location if the 16th slot has an item in it, and dumps
     whatever slots are configured to be dumped
     :param nav_turtle: The turtle object
     :param dump_spot: A coordinate directly above a chest
     :param dump_slots: What slots to dump
+    :param trigger_slot: If this slot has an item, then the turtle will dump.
     """
-    if turtle.getItemCount(16) > 0:
+    if turtle.getItemCount(trigger_slot) > 0:
         nav_turtle.move_toward(to_pos=dump_spot)
         for slot_id in dump_slots:
             item_count = turtle.getItemCount(slot_id)
