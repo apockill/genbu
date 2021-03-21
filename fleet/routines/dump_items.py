8b3cda800be081a8419ee4395b64ba92fcf1df91
Alex <alex.thiel@aotu.ai>
2021-03-20 18:37:10 -0700

    Fix dump_items routine

--- fleet/routines/dump_items.py
+++ fleet/routines/dump_items.py
@@ -1,6 +1,6 @@
 from cc import turtle
 
-from fleet.navigation_turtle import NavigationTurtle, Direction
+from fleet.navigation_turtle import NavigationTurtle, Direction, lua_errors
 
 
 def dump_if_full(nav_turtle: NavigationTurtle, dump_spot, dump_slots,
@@ -17,4 +17,12 @@ def dump_if_full(nav_turtle: NavigationTurtle, dump_spot, dump_slots,
         for slot_id in dump_slots:
             item_count = turtle.getItemCount(slot_id)
             nav_turtle.select(slot_id)
-            nav_turtle.drop_in_direction(Direction.down, item_count)
+            # Don't end the step here so in one fell swoop all slots can be
+            # cleared. Otherwise we'd have to keep state as to which slot has
+            # been cleared thus far.
+            nav_turtle.drop_in_direction(Direction.down, item_count,
+                                         end_step=False)
+
+        # Always default to selecting 1, because any 'suck' or 'dig' operation
+        # after this will go right into the trigger slot
+        nav_turtle.select(1)
--- programs/tree_farm.py
+++ programs/tree_farm.py
@@ -24,9 +24,10 @@ from fleet import (
 class TreeFarmBot(NavigationTurtle):
     NODE_MIN_DISTANCE_TO_DIG = 1
     """The distance from which the turtle is allowed to dig near a node"""
-
+    # TODO: remove prints
     TREE_REGEXES = [
         ".*spruce.*",
+        ".*log.*",
         ".*planks.*"
     ]
 
@@ -91,9 +92,24 @@ class TreeFarmBot(NavigationTurtle):
         self.state.tree_nodes = StateAttr(
             self.state, "tree_nodes",
             default=[])
+        self.state.confirmed_not_tree = StateAttr(
+            self.state, "confirmed_not_tree",
+            default=[])
         self.state.last_checkup = StateAttr(
             self.state, "last_checkup",
             default=time())
+
+    @property
+    def destructive(self):
+        """If True, the turtle is safe to dig!"""
+        pos = self.state.map.read().position
+        within_y_bounds = self.state.farm_height.read() - 1 <= pos[1]
+        within_farm_bounds = math_utils.within_bounding_points(
+            point=(pos[0], pos[2]),
+            bp1=self.state.farm_x1z1.read(),
+            bp2=self.state.farm_x2z2.read()
+        )
+        return within_y_bounds and within_farm_bounds
 
     def step(self, state: StateFile):
         fuel_loc = state.fuel_loc.read()
@@ -101,101 +117,151 @@ class TreeFarmBot(NavigationTurtle):
         last_checkup_time = state.last_checkup.read()
         check_every_n_seconds = state.check_every_n_seconds.read()
 
-        # TODO: ensure inventory
+        # Basic maintenance
+        routines.maybe_refuel(self, fuel_loc)
+        routines.dump_if_full(self, dump_loc, range(4, 17))
 
-        # Priority 0: Make sure trees are planted in the first place
+        # Resupply on dirt or saplings as necessary
+        self.maybe_resupply(state)
+
+        # Priority 0: Make sure any tree chopping tasks are finished
+        self.chop_trees(state)
+
+        # Priority 1: Make sure trees are planted in the first place
         self.plant_trees(state)
 
-        # Priority 1: Make sure
-        # tree_nodes = self.scan_for_nodes(state)
-        # self.travel_along_node(state)
+        # Priority 2: Wait for trees to grow
+        if time() - last_checkup_time < check_every_n_seconds:
+            self.move_toward(fuel_loc, destructive=self.destructive)
+            print("Waiting...")
+            os.sleep(30)
+            raise StepFinished
+        else:
+            print("Writing tree nodes!")
+            state.tree_nodes.write(self.get_tree_corners(height_offset=4))
+            state.placed_dirt.write([])
+            state.placed_saplings.write([])
+            state.confirmed_not_tree.write([])
+            state.last_checkup.write(time())
 
-    def travel_along_node(self, state):
-        """Travel along any known tree nodes"""
+    def maybe_resupply(self, state):
+        """This will resupply if the turtle is running out of materials"""
+        dirt_slot = self.inventory.slot(self.DIRT_SLOT)
+        sapling_slot = self.inventory.slot(self.SAPLING_SLOT)
+        # raise ValueError(f"yeet {dirt_slot.count} {sapling_slot.count}")
+        if dirt_slot.count <= 1:
+            self.move_toward(state.dirt_loc.read(),
+                             destructive=self.destructive)
+            self.suck_in_direction(Direction.down, end_step=False)
+            dirt_slot.refresh()
+
+        if sapling_slot.count <= 1:
+            self.move_toward(state.sapling_loc.read(),
+                             destructive=self.destructive)
+            self.suck_in_direction(Direction.down, end_step=False)
+            sapling_slot.refresh()
+
+    def chop_trees(self, state):
         tree_nodes: List[List[int]] = state.tree_nodes.read()
-        curr_pos = state.map.read().position
 
-        if not len(tree_nodes):
+        if len(tree_nodes) == 0:
+            # If theres no tree cutting tasks to be done
             return
 
+        curr_pos = state.map.read().position
         distance = partial(math_utils.turtle_distance, curr_pos)
         tree_nodes.sort(key=distance)
         next_node = tree_nodes[0]
-
-        safe_to_dig = distance(next_node) <= self.NODE_MIN_DISTANCE_TO_DIG
-        self.move_toward(to_pos=next_node, destructive=safe_to_dig)
+        self.scan_for_nodes(state)
+        self.move_toward(to_pos=next_node, destructive=self.destructive)
+        raise StepFinished
 
     def plant_trees(self, state):
         """Ensure that all trees are planted"""
-        tree_locations = self.get_tree_planting_locations(state)
-        placed_dirt = state.placed_dirt.read()
-        for tree_location in tree_locations:
-            self.plant_tree(state, tree_location)
-
-    def plant_tree(self, state, dirt_location):
         placed_dirt_locations = state.placed_dirt.read()
         placed_sapling_locations = state.placed_saplings.read()
 
+        for tree_location in self.tree_planting_locations:
+            self.plant_tree(
+                state=state,
+                dirt_location=tree_location,
+                placed_dirt_locations=placed_dirt_locations,
+                placed_sapling_locations=placed_sapling_locations)
+
+    def plant_tree(self, state, dirt_location, placed_dirt_locations,
+                   placed_sapling_locations):
+
         # First place the dirt, if necessary
-        if dirt_location not in placed_dirt_locations:
-            self.move_toward(dirt_location)
-            try:
-                self.select(self.DIRT_SLOT)
-                self.place_in_direction(Direction.down)
-            except lua_errors.BlockNotPlaceableError:
-                placed_dirt_locations.append(dirt_location)
-                with state:
-                    state.placed_dirt.write(placed_dirt_locations)
+        self.maybe_place_item(
+            state=state,
+            slot=self.DIRT_SLOT,
+            item_location=dirt_location,
+            placed_locations=placed_dirt_locations,
+            state_var=state.placed_dirt)
 
         # Then, place the sapling, if necessary
         sapling_location = [dirt_location[0],
                             dirt_location[1] + 1,
                             dirt_location[2]]
-        if sapling_location not in placed_sapling_locations:
-            self.move_toward(sapling_location)
-            try:
-                self.select(self.SAPLING_SLOT)
-                self.place_in_direction(Direction.down)
-            except lua_errors.BlockNotPlaceableError:
-                placed_sapling_locations.append(sapling_location)
-                with state:
-                    state.placed_saplings.write(placed_sapling_locations)
+        self.maybe_place_item(
+            state=state,
+            slot=self.SAPLING_SLOT,
+            item_location=sapling_location,
+            placed_locations=placed_sapling_locations,
+            state_var=state.placed_saplings)
 
     def maybe_place_item(self, state: StateFile,
                          slot: int,
                          item_location: List[int],
                          placed_locations: List[List[int]],
                          state_var):
+
         if item_location not in placed_locations:
             self.move_toward(item_location, destructive=self.destructive)
             self.select(slot)
-            self.dig_in_direction(Direction.down, end_step=False)
+            try:
+                self.dig_in_direction(Direction.down, end_step=False)
+            except lua_errors.NoItemToDigError:
+                pass
+
             self.place_in_direction(Direction.down, end_step=False)
             placed_locations.append(item_location)
             with state:
                 state_var.write(placed_locations)
 
-    @property
     @lru_cache
-    def tree_planting_locations(self):
-        """Generates a list of locations where trees should be planted"""
-        x1z1 = state.farm_x1z1.read()
-        x2z2 = state.farm_x2z2.read()
-        spacing = state.space_between_trees.read() + 1
-        tree_width = state.tree_width.read()
-        height = state.farm_height.read()
+    def get_tree_corners(self, height_offset=0) -> List[List[int]]:
+        """Return the corner of each tree planting location
+        :param height_offset: Adds an offset to the height. This is useful so
+        when the robot goes scanning tree nodes, it starts at a slightly higher
+        height
+        """
+        x1z1 = self.state.farm_x1z1.read()
+        x2z2 = self.state.farm_x2z2.read()
+        spacing = self.state.space_between_trees.read() + 1
+        tree_width = self.state.tree_width.read()
+        height = self.state.farm_height.read()
 
         from_x, to_x = sorted([x1z1[0], x2z2[0]])
         from_z, to_z = sorted([x1z1[1], x2z2[1]])
 
         return list(
-            [x + offset_x, height, z + offset_z]
-            for x in
-            range(from_x + spacing, to_x + 1 - spacing,
-                  spacing + tree_width - 1)
-            for z in
-            range(from_z + spacing, to_z + 1 - spacing,
-                  spacing + tree_width - 1)
+            [x, height + height_offset, z]
+            for x in range(from_x + spacing, to_x + 1 - spacing,
+                           spacing + tree_width - 1)
+            for z in range(from_z + spacing, to_z + 1 - spacing,
+                           spacing + tree_width - 1)
+        )
+
+    @property
+    @lru_cache
+    def tree_planting_locations(self):
+        """Generates a list of locations where trees should be planted"""
+        tree_width = self.state.tree_width.read()
+
+        return list(
+            [x + offset_x, h, z + offset_z]
+            for x, h, z in self.get_tree_corners()
             for offset_x in range(0, tree_width)
             for offset_z in range(0, tree_width)
         )
@@ -203,6 +269,7 @@ class TreeFarmBot(NavigationTurtle):
     def scan_for_nodes(self, state):
         map = state.map.read()
         tree_nodes = state.tree_nodes.read()
+        confirmed_not_tree = state.confirmed_not_tree.read()
 
         # Get rid of any nodes that match the current position
         if map.position.tolist() in tree_nodes:
@@ -215,19 +282,21 @@ class TreeFarmBot(NavigationTurtle):
         ]
 
         for direction in scan_directions:
-            block = self.inspect_in_direction(direction)
             block_position = math_utils.coordinate_in_turtle_direction(
                 curr_pos=map.position,
                 curr_angle=map.direction,
                 direction=direction).tolist()
-
+            if block_position in confirmed_not_tree:
+                continue
             if block_position in tree_nodes:
+                # This block is already known to be a tree! No need to waste
+                # time running inspect()
                 continue
-            elif block is None and block_position in tree_nodes:
-                index = tree_nodes.index(block_position)
-                tree_nodes.remove(index)
-                continue
-            elif block is None:
+            block = self.inspect_in_direction(direction)
+
+            if block is None:
+                # This block is air, and isn't known to be a tree
+                confirmed_not_tree.append(block_position)
                 continue
 
             matches_regex = block_info.name_matches_regexes(
@@ -236,11 +305,13 @@ class TreeFarmBot(NavigationTurtle):
 
             if matches_regex:
                 tree_nodes.append(block_position)
-                neighbors = math_utils.get_coordinate_neighbors(block_position)
-                for neighbor in neighbors:
-                    tree_nodes.append(neighbor.tolist())
-
-        state.tree_nodes.write(tree_nodes)
+                # TODO: Am I sure we don't necessarily want to add the neighbors?
+                # neighbors = math_utils.get_coordinate_neighbors(block_position)
+                # for neighbor in neighbors:
+                #     tree_nodes.append(neighbor.tolist())
+        with state:
+            state.tree_nodes.write(tree_nodes)
+            state.confirmed_not_tree.write(confirmed_not_tree)
         return tree_nodes
 
 
