036d15023a057984d2972ec55ff0110fcda186cf
Alex <alex.thiel@aotu.ai>
2021-03-20 18:38:29 -0700

    Use a constant for the turtles fuel limit, and use lua_errors to run it

--- fleet/routines/fuel.py
+++ fleet/routines/fuel.py
@@ -1,15 +1,17 @@
 from cc import turtle
 
-from fleet import NavigationTurtle, Direction
+from fleet import NavigationTurtle, Direction, lua_errors
 
 FUEL_SLOT = 1
+TURTLE_FUEL_LIMIT = 100000
 
 
 def maybe_refuel(nav_turtle: NavigationTurtle, refuel_spot):
     """Top off fuel if necessary. This function roughly predicts the distance to
     refuel and decides if it's necessary to refuel"""
+    fuel_level = lua_errors.run(turtle.getFuelLevel)
 
-    if turtle.getFuelLevel() < turtle.getFuelLimit() * 0.5:
+    if fuel_level < TURTLE_FUEL_LIMIT * 0.5:
         # Whether we are consuming fuel or getting it from a chest, we always
         # want to do so from the fuel slot
         nav_turtle.select(FUEL_SLOT)
